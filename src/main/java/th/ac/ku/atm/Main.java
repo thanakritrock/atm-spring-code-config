911392edaf5ebc2c059ea21522b55a448a62e672
Thanakrit Lochanti <thanakrit.lo@ku.th>
2020-09-14 18:36:58 +0700

    init and add all file to github

---
+++ src/main/java/th/ac/ku/atm/Main.java
@@ -0,0 +1,15 @@
+package th.ac.ku.atm;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(ATMConfig.class);
+        ATM atm = context.getBean(ATM.class);
+        AtmUI atmUI = new AtmUI(atm);
+        atmUI.run();
+    }
+}
